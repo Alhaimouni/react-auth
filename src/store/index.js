95cd5e5d44c7ccab3e699ef41670e8bfb93d7c0d
Mohammad Al-Haimouni <108072402+Alhaimouni@users.noreply.github.com>
2022-11-21 22:55:22 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,15 @@
 import { combineReducers, createStore } from 'redux';
+import { authReducer } from '../reducers/authReducer';
+import { dataReducer } from '../reducers/dataReduer';
 
 
-const allCombinedReducers = combineReducers({});
+
+const allCombinedReducers = combineReducers({ dataReducer, authReducer });
+
+
+
+
+
 
 
 export default function store() {
